1cc50580d0a7596cac7aafd242a30e0237446f8d
Pavel <mbsoftmb@gmail.com>
2024-01-24 23:19:15 +0300

    Commit2

--- Yolo.py
+++ Yolo.py
@@ -5,4 +5,4 @@ model = YOLO("yolov8n.pt")  # загрузите предварительно о
 #model.train(data="coco128.yaml")  # обучите модель
 #model.val()  # оцените производительность модели на наборе проверки
 model.predict(source="C:/Users/Pavel/Downloads/cars(720p).mp4", save=True)  # предсказать по изображению или видео
-model.export(format="onnx")  # экспортируйте модель в формат ONNX
+model.export(format="onnx")  # экспортируйте модель в формат ONNX 1
