8baa368d1b403ee7056f84440a24a1ff16f75b77
Pavel <mbsoftmb@gmail.com>
2024-01-24 15:02:27 +0300

    Commit

---
+++ Yolo.py
@@ -0,0 +1,8 @@
+from ultralytics import YOLO
+
+model = YOLO("yolov8n.pt")  # загрузите предварительно обученную модель YOLOv8n
+
+#model.train(data="coco128.yaml")  # обучите модель
+#model.val()  # оцените производительность модели на наборе проверки
+model.predict(source="C:/Users/Pavel/Downloads/cars(720p).mp4", save=True)  # предсказать по изображению
+model.export(format="onnx")  # экспортируйте модель в формат ONNX
